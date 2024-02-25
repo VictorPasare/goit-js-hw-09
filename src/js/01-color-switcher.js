fe1b2c3e2c6b4dfe37d1fdc13f086f0b77387886
VictorPasare <victor.pasare95@gmail.com>
2024-02-25 15:34:25 +0200

    Update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+stopBtn.setAttribute("disabled", "");
+const body = document.querySelector("body");
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null
+
+startBtn.addEventListener("click", () => {
+  startBtn.setAttribute("disabled", "");
+  stopBtn.removeAttribute("disabled");
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+})
+
+stopBtn.addEventListener("click", () => {
+  startBtn.removeAttribute("disabled");
+  stopBtn.setAttribute("disabled", "");
+  clearInterval(timerId);
+})
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,50 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const formBtn = document.querySelector('button');
+
+let position = 0;
+
+const {
+  elements: { delay, step, amount },
+} = form;
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const random = Math.random();
+      console.log(random);
+      const shouldResolve = random > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+function handleSubmit(event) {
+  event.preventDefault();
+  formBtn.setAttribute('disabled', '');
+  for (let i = 0; i < amount.value; i++) {
+    createPromise(i + 1, Number(delay.value) + Number(step.value) * i)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      })
+      .finally(() => {
+          if (i === Number(amount.value) - 1) {
+            formBtn.removeAttribute('disabled');
+          }
+        },
+      );
+  }
+  //form.reset();
+}
+
+
+form.addEventListener('submit', handleSubmit);
